79ecc0b61d2287d57e4190760c3c654951e6b20b
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-04-25 13:54:53 +0900

    refactor: tabBar 코드 리펙토링

--- assignmentYoutube/assignmentYoutube/Scene/Main/MainViewController.swift
+++ assignmentYoutube/assignmentYoutube/Scene/Main/MainViewController.swift
@@ -8,8 +8,16 @@
 import UIKit
 
 final class MainViewController: UITabBarController {
+    private let factory = ModuleFactory.resolve()
+    private lazy var tabBarVC = [
+        "Home" : factory.instantiateHomeVC(),
+        "Short" : factory.instantiateShortVC(),
+        "Add" : factory.instantiateAddVC(),
+        "Subscribe" : factory.instantiateSubscribeVC(),
+        "Storage" : factory.instantiateStorageVC()
+    ]
+    
     init() {
-        print("MainViewController-----1")
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -19,47 +27,23 @@ final class MainViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("MainViewController-----2")
+        var navigationVCs: [UINavigationController] = []
         
-        let homeViewController = HomeViewController()
-        let shortViewController = ShortViewController()
-        let addViewController = AddViewController()
-        let subscribeViewController = SubscribeViewController()
-        let storageViewController = StorageViewController()
+        YoutubeTabBar.allCases.forEach { tabInfo in
+            guard let vc = tabBarVC[tabInfo.title] else {
+                print("not match tabBar title")
+                return
+            }
+            
+            let tabBarItem = UITabBarItem(title: tabInfo.title, image: UIImage(named: tabInfo.icon), tag: 0)
+            vc.tabBarItem = tabBarItem
+            vc.navigationItem.largeTitleDisplayMode = .always
+            
+            let navigationVC = UINavigationController(rootViewController: vc)
+            navigationVC.navigationBar.prefersLargeTitles = true
+            navigationVCs.append(navigationVC)
+        }
         
-        homeViewController.title = "Home"
-        shortViewController.title = "Short"
-        addViewController.title = "Add"
-        subscribeViewController.title = "Subscribe"
-        storageViewController.title = "storage"
-        
-        homeViewController.tabBarItem.image = UIImage.init(systemName: "homeIcon")
-        shortViewController.tabBarItem.image = UIImage.init(systemName: "shortsIcon")
-        addViewController.tabBarItem.image = UIImage.init(systemName: "plueCircleIcon")
-        subscribeViewController.tabBarItem.image = UIImage.init(systemName: "subscriptionsIcon")
-        storageViewController.tabBarItem.image = UIImage.init(systemName: "LibraryIcon")
-        
-        // 위에 타이틀 text를 항상 크게 보이게 설정
-        homeViewController.navigationItem.largeTitleDisplayMode = .always
-        shortViewController.navigationItem.largeTitleDisplayMode = .always
-        addViewController.navigationItem.largeTitleDisplayMode = .always
-        subscribeViewController.navigationItem.largeTitleDisplayMode = .always
-        storageViewController.navigationItem.largeTitleDisplayMode = .always
-        
-        // navigationController의 root view 설정
-        let navigationHome = UINavigationController(rootViewController: homeViewController)
-        let navigationShort = UINavigationController(rootViewController: shortViewController)
-        let navigationAdd = UINavigationController(rootViewController: addViewController)
-        let navigationSubscribe = UINavigationController(rootViewController: subscribeViewController)
-        let navigationStorage = UINavigationController(rootViewController: storageViewController)
-        
-        navigationHome.navigationBar.prefersLargeTitles = true
-        navigationShort.navigationBar.prefersLargeTitles = true
-        navigationAdd.navigationBar.prefersLargeTitles = true
-        navigationSubscribe.navigationBar.prefersLargeTitles = true
-        navigationStorage.navigationBar.prefersLargeTitles  = true
-        
-        self.setViewControllers([navigationHome, navigationShort, navigationAdd, navigationSubscribe, navigationStorage], animated: false)
-        
+        self.setViewControllers(navigationVCs, animated: true)
     }
 }
